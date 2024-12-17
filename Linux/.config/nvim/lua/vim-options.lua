0e2d79e1fa6b55bff5a16dbae82f0dffc03c7e26
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2024-12-17 03:39:58 -0500

    tmux conf and nvim conf changed

---
+++ Linux/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,7 @@
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set mouse=a")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
