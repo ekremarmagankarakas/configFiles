aaafb7f643aa7306a287462c7f294271680be6ab
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2025-10-15 23:57:06 -0400

    added telescope improvements

--- Linux/dotfiles/nvim/lua/plugins/telescope.lua
+++ Linux/dotfiles/nvim/lua/plugins/telescope.lua
@@ -19,6 +19,22 @@ return {
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
             vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
 			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files" })
+
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+            vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymaps" })
+            vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
+            vim.keymap.set("n", "<leader>fC", builtin.command_history, { desc = "Command History" })
+            vim.keymap.set("n", "<leader>fs", builtin.current_buffer_fuzzy_find, { desc = "Search in Buffer" })
+            vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Marks" })
+            vim.keymap.set("n", "<leader>fj", builtin.jumplist, { desc = "Jumplist" })
+            vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "Registers" })
+            vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Treesitter Symbols" })
+            vim.keymap.set("n", "<leader>f/", builtin.search_history, { desc = "Search History" })
+            vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Quickfix List" })
+            vim.keymap.set("n", "<leader>fl", builtin.loclist, { desc = "Location List" })
+            
+            vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word Under Cursor" })
+
 			vim.keymap.set("n", "<leader>fa", function()
 				require("telescope.builtin").find_files({
 					prompt_title = "Search in Selected Dirs",
