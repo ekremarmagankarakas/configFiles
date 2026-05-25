bf63c5e93bb36b82721418b473ef73d81209db6e
ekremarmagankarakas <ekremarmagankarakas@gmail.com>
2026-05-25 17:53:14 -0400

    gitdiffview cleanup

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -177,128 +177,24 @@ return {
 			{ "<leader>gdt", "<cmd>DiffviewToggleFiles<cr>", desc = "Toggle File Panel" },
 		},
 		opts = {
-			diff_binaries = false,
-			enhanced_diff_hl = false,
-			use_icons = true,
-			show_help_hints = true,
-			watch_index = true,
-
-			icons = {
-				folder_closed = "",
-				folder_open = "",
-			},
-
-			signs = {
-				fold_closed = "",
-				fold_open = "",
-				done = "✓",
-			},
-
-			view = {
-				default = {
-					layout = "diff2_horizontal",
-					winbar_info = false,
-				},
-				merge_tool = {
-					layout = "diff3_horizontal",
-					disable_diagnostics = true,
-					winbar_info = true,
-				},
-				file_history = {
-					layout = "diff2_horizontal",
-					winbar_info = false,
-				},
-			},
-
-			file_panel = {
-				listing_style = "tree",
-				tree_options = {
-					flatten_dirs = true,
-					folder_statuses = "only_folded",
-				},
-				win_config = {
-					position = "left",
-					width = 35,
-					win_opts = {},
-				},
-			},
-
-			file_history_panel = {
-				log_options = {
-					git = {
-						single_file = {
-							diff_merges = "combined",
-						},
-						multi_file = {
-							diff_merges = "first-parent",
-						},
-					},
-				},
-				win_config = {
-					position = "bottom",
-					height = 16,
-					win_opts = {},
-				},
-			},
-
-			commit_log_panel = {
-				win_config = {
-					win_opts = {},
-				},
-			},
-
-			default_args = {
-				DiffviewOpen = {},
-				DiffviewFileHistory = {},
-			},
-
 			hooks = {
 				diff_buf_read = function(bufnr)
-					-- Change local options in diff buffers
 					vim.opt_local.wrap = false
 					vim.opt_local.list = false
 					vim.opt_local.relativenumber = false
 					vim.opt_local.cursorline = false
 				end,
 				view_opened = function(view)
-					-- Highlight when a view is opened
 					vim.notify("DiffView opened", vim.log.levels.INFO)
 				end,
 			},
-
 			keymaps = {
 				view = {
-					-- Conflict resolution (only active in merge views)
-					{
-						"n",
-						"<leader>dco",
-						"<cmd>lua require('diffview.actions').conflict_choose('ours')<cr>",
-						{ desc = "Choose ours" },
-					},
-					{
-						"n",
-						"<leader>dct",
-						"<cmd>lua require('diffview.actions').conflict_choose('theirs')<cr>",
-						{ desc = "Choose theirs" },
-					},
-					{
-						"n",
-						"<leader>dcb",
-						"<cmd>lua require('diffview.actions').conflict_choose('base')<cr>",
-						{ desc = "Choose base" },
-					},
-					{
-						"n",
-						"<leader>dca",
-						"<cmd>lua require('diffview.actions').conflict_choose('all')<cr>",
-						{ desc = "Choose all" },
-					},
-					{
-						"n",
-						"dx",
-						"<cmd>lua require('diffview.actions').conflict_choose('none')<cr>",
-						{ desc = "Delete conflict region" },
-					},
+					{ "n", "<leader>dco", "<cmd>lua require('diffview.actions').conflict_choose('ours')<cr>", { desc = "Choose ours" } },
+					{ "n", "<leader>dct", "<cmd>lua require('diffview.actions').conflict_choose('theirs')<cr>", { desc = "Choose theirs" } },
+					{ "n", "<leader>dcb", "<cmd>lua require('diffview.actions').conflict_choose('base')<cr>", { desc = "Choose base" } },
+					{ "n", "<leader>dca", "<cmd>lua require('diffview.actions').conflict_choose('all')<cr>", { desc = "Choose all" } },
+					{ "n", "dx", "<cmd>lua require('diffview.actions').conflict_choose('none')<cr>", { desc = "Delete conflict region" } },
 				},
 			},
 		},
